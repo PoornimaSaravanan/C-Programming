1bb93ee2199cd7cca33102668714abbff9ccbc94
PoornimaSaravanan <prnmsrvnn@gmail.com>
2026-06-03 20:53:29 +0530

    Create program 4.C

---
+++ program 4.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  char ch='a';
+  printf("character=%c\n",ch);
+  printf("ASCII Value=%d\n",ch);
+  printf("Uppercase=%c",ch-32);
+  return 0;
+}
+-------------------------------------
+Output:character=a
+       ASCII Value=97
+       Uppercase=A
