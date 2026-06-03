5ef34b13d68c5828634965a91fa34b60a46a0bfa
PoornimaSaravanan <prnmsrvnn@gmail.com>
2026-06-03 21:00:01 +0530

    Create Program 5.C

---
+++ Program 5.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+  int i=1;
+  while(i<=10)
+  {
+    printf("Hi\n");
+    i++;
+  }
+    printf("END");
+}
+--------------------------------
+
+Output:Hi
+       Hi
+       Hi 
+       Hi
+       Hi
+       Hi
+       Hi
+       Hi
+       Hi
+       Hi
+      END
