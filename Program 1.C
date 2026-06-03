d9d08b896c6366bef2f21eef0038cf2fbddaba93
PoornimaSaravanan <prnmsrvnn@gmail.com>
2026-06-03 20:57:44 +0530

    Update and rename c program to Program 1.C

---
+++ Program 1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//define pi 3.14
+int main()
+{
+  int r;
+  const float pi=3.14;
+  float area;
+  scanf("%d",&r);
+  area=(pi*r*r);
+  printf("%.2f",area);
+  return 0;
+}
+
+---------------------------------------
+
+Output:882026.06
