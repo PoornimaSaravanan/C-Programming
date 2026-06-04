9b9c69f6884f1d9731b9fe4fd65c74e2cac24e35
PoornimaSaravanan <prnmsrvnn@gmail.com>
2026-06-04 20:44:21 +0530

    Update and rename Program 7.C to Program 06.C

---
+++ Program 06.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int n = 10, i, sum = 0;
+    for(i = 1; i <= n; i++)
+    {
+      sum = sum + i;
+    }
+
+    printf("Sum of series = %d", sum);
+    return 0;
+}
+------------------------------------------------
+Output: Sum of series-55
